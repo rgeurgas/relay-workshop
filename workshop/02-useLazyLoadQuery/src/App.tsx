326ef48076e8fdcc936d07880dcad0e48addb279
Rodrigo Geurgas <rodrigogeurgas@gmail.com>
2020-08-31 17:29:55 -0300

    feat(assignment): done second assignment

--- workshop/02-useLazyLoadQuery/src/App.tsx
+++ workshop/02-useLazyLoadQuery/src/App.tsx
@@ -2,26 +2,39 @@ import React from 'react';
 
 import { Flex, Text } from 'rebass';
 import { Card, Content } from '@workshop/ui';
+import { graphql, useLazyLoadQuery } from 'react-relay/hooks'
+import { AppQuery, AppQueryVariables } from './__generated__/AppQuery.graphql'
 
 const App = () => {
-  const posts = {
-    edges: [],
-  };
+  // Added variables but there was nothing to change then on the default website don't know if 
+  // there needs to be one, previous assignment shows how to use then better
+  const variables: AppQueryVariables = { first: 10 }
 
-  /**
-   * @TODO
-   * Fetch posts to be rendered in this component using `useLazyLoadQuery`
-   */
+  const { posts } = useLazyLoadQuery<AppQuery>(
+    graphql`
+    query AppQuery($first: Int) {
+      posts(first: $first) {
+        edges {
+          node {
+            id
+            content
+          }
+        }
+      }          
+    }
+    `,
+    variables
+  )
 
   return (
     <Content>
       <Flex flexDirection='column'>
         <Text>Posts</Text>
         <Flex flexDirection='column'>
-          {posts.edges.map(({ node }) => (
-            <Card key={node.id} mt='10px' flexDirection='column' p='10px'>
-              <Text>id: {node.id}</Text>
-              <Text>content: {node.content}</Text>
+          {posts.edges.filter((el) => { return el !== null && el.node !== null }).map((el) => (
+            <Card key={el!!.node!!.id} mt='10px' flexDirection='column' p='10px'>
+              <Text>id: {el!!.node!!.id}</Text>
+              <Text>content: {el!!.node!!.content}</Text>
             </Card>
           ))}
         </Flex>
--- workshop/02-useLazyLoadQuery/src/Root.tsx
+++ workshop/02-useLazyLoadQuery/src/Root.tsx
@@ -2,17 +2,53 @@ import React from 'react';
 
 import Providers from './Providers';
 import App from './App';
+import Loading from './Loading';
+
+/**
+ * Interface for Errors
+ */
+interface ErrorBoundaryState {
+  error: Error | null;
+}
+
+/**
+ * Default error boundary Component
+ */
+export class ErrorBoundary extends React.Component<
+  {
+    children: React.ReactNode;
+    fallback: (error: Error) => any;
+  },
+  ErrorBoundaryState
+  > {
+  state: ErrorBoundaryState = {
+    error: null,
+  };
+
+  componentDidCatch(error: Error) {
+    this.setState({ error: error });
+  }
+
+  render() {
+    const { children, fallback } = this.props;
+    const { error } = this.state;
+
+    if (error) {
+      return React.createElement(fallback, error);
+    }
+
+    return children;
+  }
+}
 
 const Root = () => {
-  /**
-   * @TODO
-   * Add Suspense to suspend when using useLazyLoadQuery
-   * Add ErrorBoundary to catch errors in useLazyLoadQuery
-   */
-
   return (
     <Providers>
-      <App />
+      <ErrorBoundary fallback={(err) => `Error: ${err.message}: ${err.stack}`}>
+        <React.Suspense fallback={Loading()}>
+          <App />
+        </React.Suspense>
+      </ErrorBoundary>
     </Providers>
   );
 };
