138a091961d1b880cc56a980923c2cb6637e074f
Rodrigo Geurgas <rodrigogeurgas@gmail.com>
2020-09-03 18:55:23 -0300

    feat(assignment): done tenth assignment

--- workshop/10-testUsePreloadQuery/src/components/feed/post/__tests__/PostDetail.spec.tsx
+++ workshop/10-testUsePreloadQuery/src/components/feed/post/__tests__/PostDetail.spec.tsx
@@ -1,13 +1,14 @@
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
-import { MockPayloadGenerator, createMockEnvironment } from 'relay-test-utils';
+import { MockPayloadGenerator } from 'relay-test-utils';
 
 import { preloadQuery } from 'react-relay/hooks';
 
 import { JSResource } from '@workshop/route';
 
 import PostDetail from '../PostDetail';
+import { Environment } from '../../../../relay';
 
 import { withProviders } from '../../../../../test/withProviders';
 
@@ -36,18 +37,9 @@ it('should render post like button', async () => {
     }),
   };
 
-  /**
-   * TODO
-   * queue a pending operation, this would be a preloadQuery call
-   */
-  const environment = createMockEnvironment();
-  environment.mock.queueOperationResolver(operation => MockPayloadGenerator.g);
-  Environment.queuePendingOperation;
+  Environment.mock.queuePendingOperation(query, variables);
 
-  /**
-   * TODO
-   * mock a queued operation
-   */
+  Environment.mock.queueOperationResolver(op => MockPayloadGenerator.generate(op, customMockResolvers));
 
   const Root = withProviders({
     routes,
@@ -55,17 +47,67 @@ it('should render post like button', async () => {
     Component: PostDetail,
   });
 
+
   const prepared = {
-    /**
-     * TODO
-     * preload query
-     */
-    postDetailQuery: {},
+    postDetailQuery: preloadQuery(
+      Environment,
+      PostDetailQuery,
+      variables,
+      { fetchPolicy: 'store-or-network' }
+    ),
   };
 
   const { debug, getByText } = render(<Root prepared={prepared} />);
 
-  debug();
-
   expect(getByText('Welcome to React Europe')).toBeTruthy();
+});
+
+it('should not find post', async () => {
+  const postId = 'postId';
+
+  const routes = [
+    {
+      component: JSResource('Component', () => new Promise(resolve => resolve(PostDetail))),
+      path: '/post/:id',
+    },
+  ];
+
+  const initialEntries = [`/post/${postId}`];
+
+  const PostDetailQuery = require('../__generated__/PostDetailQuery.graphql');
+
+  const query = PostDetailQuery;
+  const variables = {
+    id: postId,
+  };
+
+  const customMockResolvers = {
+    Post: () => ({
+      content: 'Welcome to React Europe',
+    }),
+  };
+
+  Environment.mock.queuePendingOperation(query, variables);
+
+  Environment.mock.queueOperationResolver(op => MockPayloadGenerator.generate(op, customMockResolvers));
+
+  const Root = withProviders({
+    routes,
+    initialEntries,
+    Component: PostDetail,
+  });
+
+
+  const prepared = {
+    postDetailQuery: preloadQuery(
+      Environment,
+      PostDetailQuery,
+      variables,
+      { fetchPolicy: 'store-or-network' }
+    ),
+  };
+
+  const { debug, queryByText } = render(<Root prepared={prepared} />);
+
+  expect(queryByText('Not valid post')).toBeNull();
 });
