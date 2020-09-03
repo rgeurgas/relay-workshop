053ee9a60451e1d94c14c55acecfe5e205fa4e5e
Rodrigo Geurgas <rodrigogeurgas@gmail.com>
2020-09-03 17:35:35 -0300

    feat(assignment): first version

--- workshop/10-testUsePreloadQuery/src/components/feed/post/__tests__/PostDetail.spec.tsx
+++ workshop/10-testUsePreloadQuery/src/components/feed/post/__tests__/PostDetail.spec.tsx
@@ -1,16 +1,12 @@
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import React from 'react';
-// eslint-disable-next-line
-import { MockPayloadGenerator } from 'relay-test-utils';
+import { MockPayloadGenerator, createMockEnvironment } from 'relay-test-utils';
 
-// eslint-disable-next-line
 import { preloadQuery } from 'react-relay/hooks';
 
 import { JSResource } from '@workshop/route';
 
-// eslint-disable-next-line
-import { Environment } from '../../../../relay';
 import PostDetail from '../PostDetail';
 
 import { withProviders } from '../../../../../test/withProviders';
@@ -29,26 +25,24 @@ it('should render post like button', async () => {
 
   const PostDetailQuery = require('../__generated__/PostDetailQuery.graphql');
 
-  // eslint-disable-next-line
   const query = PostDetailQuery;
-  // eslint-disable-next-line
   const variables = {
     id: postId,
   };
 
-  /**
-   * TODO
-   * mock content of Post
-   */
-  // eslint-disable-next-line
   const customMockResolvers = {
-    Post: () => ({}),
+    Post: () => ({
+      content: 'Welcome to React Europe'
+    }),
   };
 
   /**
    * TODO
    * queue a pending operation, this would be a preloadQuery call
    */
+  const environment = createMockEnvironment()
+  environment.mock.queueOperationResolver(operation => MockPayloadGenerator.g)
+  Environment.queuePendingOperation
 
   /**
    * TODO
@@ -69,10 +63,8 @@ it('should render post like button', async () => {
     postDetailQuery: {},
   };
 
-  // eslint-disable-next-line
   const { debug, getByText } = render(<Root prepared={prepared} />);
 
-  // uncomment to check DOM
   debug();
 
   expect(getByText('Welcome to React Europe')).toBeTruthy();
